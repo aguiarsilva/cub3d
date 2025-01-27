bf38a33ddfb2442ff2377bedc5b95ffa5b11fca7
Bruno Aguiar <aguiarsilva@gmail.com>
2025-01-27 20:54:19 +0100

    insert function to put pixel

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,6 +3,9 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+#define HEIGHT 720
+#define WIDTH 1280
+
 #include "../includes/minilibx-linux/mlx.h"
 #include <stdio.h>
 #include <math.h>
@@ -14,6 +17,12 @@ typedef struct s_game
     void    *mlx;
     void    *win;
     void    *img;
+
+    char    *data;
+    int     bpp;
+    int     line_size;
+    int     endian;
+    
 }   t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,8 +4,34 @@
 void game_init(t_game *game)
 {
     game->mlx = mlx_init();
-    game->win = mlx_new_window(game->mlx, 1280, 720, "Cub3d");
-    game->img = mlx_new_image(game->mlx, 1280, 720);
+    game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Cub3d");
+    game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+}
+
+void put_pixel(int x, int y, int color, t_game *game)
+{
+    int idx;
+
+    if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+        return;
+
+    idx = y * game->line_size + x * game->bpp / 8;
+    game->data[idx] = color & 0xFF;
+    game->data[idx + 1] = (color >> 8) & 0xFF;
+    game->data[idx + 2] = (color >> 16) & 0xFF;
+}
+
+void make_square(int x, int y, int size, int color, t_game *game)
+{
+    for (int i = 0; i < size; i++)
+        put_pixel(x + i, y, color, game);
+    for (int i = 0; i < size; i++)
+        put_pixel(x, y + i, color, game);
+    for (int i = 0; i < size; i++)
+        put_pixel(x + size, y + i, color, game);
+    for (int i = 0; i < size; i++)
+        put_pixel(x + i, y + size, color, game);
+
 }
 
 int main(void)
@@ -13,7 +39,7 @@ int main(void)
     t_game  game;
 
     game_init(&game);
-
+    make_square(WIDTH / 2, HEIGHT / 2, 10, 0xFF0000, &game);
     mlx_loop(game.mlx);
 
     return 0;
