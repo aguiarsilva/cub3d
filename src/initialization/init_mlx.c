b1201a3b909c9e6dd7ebff717b8c67d7f0ea9492
Bruno Aguiar <aguiarsilva@gmail.com>
2025-03-25 12:51:54 +0100

    refactored initialization mlx

--- src/initialization/init_mlx.c
+++ src/initialization/init_mlx.c
@@ -26,19 +26,34 @@ void	ft_initialize_image(t_game_data *game_data, t_img_data *image, int width, i
 
 void	ft_initialize_texture_img(t_game_data *game_data, t_img_data *image, char *path)
 {
-	ft_initialize_img_data(image);
-	image->img = mlx_xpm_file_to_image(game_data->mlx, path, &game_data->texture_data.size,
-			&game_data->texture_data.size);
-	if (image->img == NULL)
+	// ft_initialize_img_data(image);
+	// image->img = mlx_xpm_file_to_image(game_data->mlx, path, &game_data->texture_data.size,
+	// 		&game_data->texture_data.size);
+	// if (image->img == NULL)
+	// 	ft_clean_and_exit(game_data, ft_error_msg(NULL, ERR_MLX_IMG, 1));
+	// image->addr = (int *)mlx_get_data_addr(image->img, &image->pixel_bits,
+	// 		&image->size_line, &image->endian);
+	// return ;
+	int		size;
+
+	if (!game_data || !image || !path || !game_data->mlx)
 		ft_clean_and_exit(game_data, ft_error_msg(NULL, ERR_MLX_IMG, 1));
+	ft_initialize_img_data(image);
+	size = game_data->texture_data.size;
+	image->img = mlx_xpm_file_to_image(game_data->mlx, path, &size, &size);
+	if (!image->img)
+		ft_clean_and_exit(game_data, ft_error_msg(NULL, ERR_MLX,IMG, 1));
 	image->addr = (int *)mlx_get_data_addr(image->img, &image->pixel_bits,
 			&image->size_line, &image->endian);
-	return ;
+	if (!image->addr)
+		ft_clean_and_exit(game_data, ft_error_msg(NULL, ERR_MLX_IMG, 1));
 }
 
 // initialize screen and handle basic events as describe in this milestone
 void ft_initialize_mlx_screen(t_game_data *game_data)
 {
+	if (!game_data)
+		exit(EXIT_FAILURE);
 	game_data->mlx = mlx_init();
 	if (!game_data->mlx)
 	{
