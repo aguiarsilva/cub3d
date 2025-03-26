4a2d76655187d04a9f5103e6bedea250ae4b77aa
Bruno Aguiar <aguiarsilva@gmail.com>
2025-03-26 22:30:18 +0100

    norminetted rendering folder

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -1,9 +1,7 @@
 #include "cub3d.h"
 
-//Initialialzie and render graphics
-
-
-void	ft_set_frame_image_pixel(t_game_data *game_data, t_img_data *image, int x, int y)
+void	ft_set_frame_image_pixel(t_game_data *game_data,
+	t_img_data *image, int x, int y)
 {
 	if (game_data->texture_pixels[y][x] > 0)
 		ft_set_image_pixel(image, x, y, game_data->texture_pixels[y][x]);
@@ -16,11 +14,12 @@ void	ft_set_frame_image_pixel(t_game_data *game_data, t_img_data *image, int x, 
 void	ft_generate_render_frame(t_game_data *game_data)
 {
 	t_img_data	image;
-	int		x;
-	int		y;
+	int			x;
+	int			y;
 
 	image.img = NULL;
-	ft_initialize_image(game_data, &image, game_data->win_width, game_data->win_height);
+	ft_initialize_image(game_data, &image, game_data->win_width,
+		game_data->win_height);
 	y = 0;
 	while (y < game_data->win_height)
 	{
@@ -49,21 +48,11 @@ void	ft_generate_render_images(t_game_data *game_data)
 	ft_generate_render_raycast(game_data);
 }
 
-
-int ft_render(t_game_data *game_data)
+int	ft_render(t_game_data *game_data)
 {
-    // Clear the window or perform any necessary pre-rendering steps
-    // mlx_clear_window(game_data->mlx, game_data->win);
-
-    // Render the game scene here
 	game_data->player.movement.moved += ft_move_player(game_data);
-    if (game_data->player.movement.moved == 0)
-    	return (0);
-    ft_generate_render_images(game_data);
-    
-
-    // // Display the rendered frame
-    // mlx_put_image_to_window(game_data->mlx, game_data->win, 0, 0);
-
-    return 0;
+	if (game_data->player.movement.moved == 0)
+		return (0);
+	ft_generate_render_images(game_data);
+	return (0);
 }
