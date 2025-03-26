16eb851d8b1f4476f2dfbea5370f58d621437a57
Bruno Aguiar <aguiarsilva@gmail.com>
2025-03-26 14:28:37 +0100

    norminette parse_map.c and shortened a function with more than 25 lines

--- src/parsing/check_file.c
+++ src/parsing/check_file.c
@@ -1,41 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baguiar- <baguiar-@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 12:52:17 by baguiar-          #+#    #+#             */
+/*   Updated: 2025/03/26 12:56:45 by baguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
-
-// // need refactoring less lines of code
-// int ft_file_and_dir_checker(char *arg, bool cub_file)
-// {
-//     int fd;
-//     size_t len;
-//     // bool is_dir = false;
-
-//     // Check if the argument is a directory
-//     fd = open(arg, O_DIRECTORY);
-//     if (fd >= 0)
-//     {
-//         close(fd);
-//         return ft_error_msg(arg, ERR_FILE_IS_DIR, STATUS_FAIL);
-//     }
-
-//     // Check if the file can be opened
-//     fd = open(arg, O_RDONLY);
-//     if (fd == -1)
-//         return ft_error_msg(arg, strerror(errno), STATUS_FAIL);
-//     close(fd);
-
-//     // Check file extension xpm and cup
-//     len = ft_strlen(arg);
-//     if (cub_file)
-//     {
-//         if (len < 4 || arg[len - 4] != '.' || arg[len - 3] != 'c' || arg[len - 2] != 'u' || arg[len - 1] != 'b')
-//             return ft_error_msg(arg, ERR_FILE_NOT_CUB, STATUS_FAIL);
-//     }
-//     else
-//     {
-//         if (len < 4 || arg[len - 4] != '.' || arg[len - 3] != 'x' || arg[len - 2] != 'p' || arg[len - 1] != 'm')
-//             return ft_error_msg(arg, ERR_FILE_NOT_XPM, STATUS_FAIL);
-//     }
-
-//     return STATUS_OK;
-// }
 
 bool	ft_comfirm_dir(char *arg)
 {
